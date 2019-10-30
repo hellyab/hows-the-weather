1ecbbb8733f27ec8b4621ad954142d552207ee20
hell.yab <yabsra23@gmail.com>
2019-10-30 22:39:01 +0300

    initial commit with the project progress same as the one on the tutorial

---
+++ src/components/Titles.js
@@ -0,0 +1,12 @@
+import React from "react";
+const Titles = () => (
+  <div>
+    <h1 className="title-container__title">How's the weather?</h1>
+    <p className="title-container__subtitle">
+      Find out the weather conditions of cities all across the globe.{" "}
+    </p>
+  </div>
+);
+
+export default Titles;
+
